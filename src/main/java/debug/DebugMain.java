dfd9bb8503b2c55c060eee1d07b5e8fda0f75443
Eufratesz <lokoshazi@freemail.hu>
2021-10-06 20:43:06 +0200

    First commit

---
+++ src/main/java/debug/DebugMain.java
@@ -0,0 +1,23 @@
+package debug;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class DebugMain {
+
+    public int find(List<String> words, String prefix) {
+        int count = 0;
+        for (String word : words) {
+            if (word.startsWith(prefix)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int count = new DebugMain().find(Arrays.asList("one", "two", "three", "onehundred"), "one");
+
+        System.out.println(count);
+    }
+}
