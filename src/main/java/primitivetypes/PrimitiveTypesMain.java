5f9a35fc20a3118be0d3e2164309ee9d2c115484
Eufratesz <lokoshazi@freemail.hu>
2021-10-11 21:40:37 +0200

    First commit

---
+++ src/main/java/primitivetypes/PrimitiveTypesMain.java
@@ -0,0 +1,22 @@
+package primitivetypes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrimitiveTypesMain {
+
+    public static void main(String[] args) {
+        int i = 9;
+
+        Integer j = 9;
+
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(i);
+
+        System.out.println(Integer.toString(9, 2));
+        System.out.println(Integer.toString(-9, 2));
+        System.out.println(Integer.toBinaryString(-9));
+
+        int k = Integer.parseInt("9");
+    }
+}
